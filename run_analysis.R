53368d55605d369ad562d7faa68b34c82e7a4dec
Aaron Graham <aarcg@yahoo.com>
2015-08-16 19:46:29 -0400

    Name cleaning works, split script into helper functions

--- run_analysis.R
+++ run_analysis.R
@@ -1,52 +1,31 @@
-# buildFrame()
-#
-# Take a directory and build/return a data frame:
-# 'subject'  'activity'  'feature_1'  'feature_2' ... 'feature_n'
-#
-# @param character directory, character features, character activities
-# @return data.frame
-buildFrame <- function(directory = character(),
-                       features = character(),
-                       activities = character(),
-                       subsetVector = int()) {
+source("./HelperFunctions/clean_names.R")
+source("./HelperFunctions/build_frame.R")
+runAnalysis <- function(dataDir = ".") {
   
-  # Create proper file paths
-  subjects <- file.path(directory, paste0("subject_", basename(directory), ".txt"))
-  x <- file.path(directory, paste0("X_", basename(directory), ".txt"))
-  y <- file.path(directory, paste0("y_", basename(directory), ".txt"))
-
-  # Read in the files
-  df.subjects <- read.table(subjects)
-  df.x <- read.table(x)
-  df.y <- read.table(y)
+  features <- read.table(file.path(dataDir, "features.txt"), stringsAsFactors = FALSE)
+  activities <- read.table(file.path(dataDir, "activity_labels.txt"))
+  dirs <- list.dirs(path=dataDir, recursive = FALSE)
   
-  # Set the names for the features in x
-  names(df.x) = features
+  # Format the feature names
+  featureNames <- cleanNames(features[,2])
   
-  # Subset the x dataframe
-  df.x <- df.x[,subsetVector]
+  # Build a vector to use in subsetting the dataframe
+  subsetVector <- features[grep("mean[()]|std[()]", features[,2]),1]
   
-  df.data <- data.frame("subject" = df.subjects[,1],
-                          "activity" = activities[df.y[,1]],
-                          df.x)
+  # Create a list that will hold our data frames
+  data <- list()
+  
+  # Build the data frames
+  print("Building tidy data...")
+  for(i in 1:length(dirs)) {
+    data[[i]] <- buildFrame(dirs[i], featureNames, activities[,2], subsetVector)
+  }
+  
+  # Build the complete data frame and sort it by subject
+  data.complete <- rbind(data[[1]], data[[2]])
+  data.complete <- data.complete[order(data.complete$subject), ]
+  
+  # Save the tidy data
+  print(paste("saving tidy data to", file.path(getwd(), "tidy.txt")))
+  write.table(data.complete, file = "tidy.txt", row.names = FALSE)
 }
-
-features <- read.table("features.txt")
-activities <- read.table("activity_labels.txt")
-dirs <- list.dirs(recursive = FALSE)
-subsetVector <- features[grep("mean[()]|std[()]", features[,2]),1]
-
-# Create a list that will hold our data frames
-data <- list()
-
-# Build the data frames
-for(i in 1:length(dirs)) {
-  data[[i]] <- buildFrame(dirs[i], features[,2], activities[,2], subsetVector)
-}
-
-# Build the complete data frame and sort it by subject
-data.complete <- rbind(data[[1]], data[[2]])
-data.complete <- data.complete[order(data.complete$subject), ]
-
-# Save the tidy data
-write.table(data.complete, file = "tidy.txt", row.names = FALSE)
